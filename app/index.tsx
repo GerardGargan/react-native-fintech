f40015405b396a1fbabb388518486f80add3abce
Gerard Gargan <122169905+GerardGargan@users.noreply.github.com>
2024-11-12 01:29:14 +0000

    fixed initial issues, project building

---
+++ app/index.tsx
@@ -0,0 +1,11 @@
+import { Text, View } from "react-native";
+
+const Page = () => {
+    return (
+        <View>
+            <Text>UPDATE</Text>
+        </View>
+    )
+};
+
+export default Page;
